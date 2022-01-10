dc54cf9d0dda95e063c4a30716601796a7e04193
EastsCloud <easts.cloud.xmzd@outlook.com>
2022-01-10 18:44:20 -0500

    first commit

---
+++ scripts/list.js
@@ -0,0 +1,9 @@
+/* table format:
+ * start from [1][1]
+ * three columns: first name, last name, email address
+ */
+var listr=1, listc=3;
+var list = [
+	["", "", "", ""],
+	["", "qingyang", "zhu", "qingyang.zhu@rectorywolves.com"]
+]
