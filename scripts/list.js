5b1c3ebbbacaab39323c1844d1d425b4dd3f0eb1
EastsCloud <95631880+EastsCloud@users.noreply.github.com>
2022-03-14 14:58:54 -0400

    Delete scripts directory

--- scripts/list.js
+++
@@ -1,9 +0,0 @@
-/* table format:
- * start from [1][1]
- * three columns: first name, last name, email address
- */
-var listr=1, listc=3;
-var list = [
-	["", "", "", ""],
-	["", "qingyang", "zhu", "qingyang.zhu@rectorywolves.com"]
-]
